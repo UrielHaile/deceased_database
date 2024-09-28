f8b740873684ec13cbf74566d4820e4488ba2e9b
Jose Adrian <kerpador@gmail.com>
2024-09-28 15:39:37 -0600

    add ther web-scrapper and the readme.md

--- README.md
+++ README.md
@@ -3,10 +3,19 @@ In this repository has the objective to process the data from the mexican deceas
 For this project will be required to install 
 * Pandas
 * Numpy
-
+* bs4
+* requests
+* zipfile
+* os
 >[!NOTE]
 >To install the frameworks it'll be useful to have the package manager called pip
 >
  ` pip install numpy`
 
 ` pip install pandas `
+ 
+ ` pip install bs4 `
+ 
+ ` pip install requests `
+ 
+ ` pip install os `
---
+++ scrapper.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import pandas as pd
+import zipfile
+class fetchingData():
+    def __init__(self):
+        self.url_main="http://www.dgis.salud.gob.mx"
+        self.page_to=requests.get("http://www.dgis.salud.gob.mx/contenidos/basesdedatos/da_defunciones_gobmx.html")
+        self.soup=BeautifulSoup(self.page_to.text,'html.parser')
+        self.tables=self.soup.findAll("table")
+        self.name=[h3.get_text().replace(' ','_')  for h3 in self.soup.findAll("h3")]
+    def requesting_data(self,i):
+        a=self.tables[i].findAll("a")
+        self.createFolder(i)
+        for enlace in a:
+            url=enlace.get("href")
+            if url:
+                if not url.startswith('http'):
+                    url=url[5:]
+                    url=self.url_main+url
+                # print(url)
+                archivo =requests.get(url)
+                name=url[url.rfind('/')+1:url.find('?'):1]
+                print(name)
+                with open(f'./{self.name[i]}/{name}.zip', 'wb') as f:
+                    f.write(archivo.content)
+                self.unzip(f'./{self.name[i]}/{name}.zip')
+    def createFolder(self,i):   
+        name_folder=self.name[i]     
+        if not os.path.exists(name_folder):
+            os.mkdir(name_folder)
+            print(f"Carpeta '{name_folder}' creada.")
+        else:
+            print(f"La carpeta '{name_folder}' ya existe.")
+    def unzip(self,path_zip):
+        with zipfile.ZipFile(path_zip, 'r') as zip_ref:
+            zip_ref.extractall(os.path.dirname(path_zip))  # Extrae en el mismo directorio que el ZIP
+        print(f"Archivo descomprimido: {path_zip}")
+        os.remove(path_zip)
+
+def menu():
+    i=0
+    while(i!=3):
+        print("Requesting data.....")
+        print("You will extract the following data:")
+        print("0.-Catálogos de defunciones\n1.-Campos de defunciones\n2.-Registro de defunciones\n3.-salir")
+        i=int(input("Ingresa el dato que desea extraer: "))
+        if i==3:
+            break
+        d=fetchingData()
+        d.requesting_data(i)
+menu()
